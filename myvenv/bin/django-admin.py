320e65a3c8479cc9a44a99be529d8b7481fe88ea
amarin <amarin@veritran.com>
2018-11-23 20:10:12 -0500

    Iniciando app

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amarin/Documentos/python_pereira/charla_django/app_prueba/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
