987718d31f69a68e059750c045c18732d2aa3afa
amarin <amarin@veritran.com>
2018-11-28 03:49:16 -0500

    Se crean templates de prueba y hola mundo.

--- navidad/tests.py
+++ navidad/tests.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
+
+def test_user_creation(self):
+    User(email="prueba@prueba.com", name='prueba user').save()
+
+    users = User.objects.all()
+    self.assertEquals(users.count(), 1)
--- navidad/views.py
+++ navidad/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# def home(request):
+#     return HttpResponse('Hola feliz navidad!!')
+
+
+def home(request):
+    mensaje = 'Hola Feliz navidad'
+    return render(request, 'navidad/prueba_1.html', {'mensaje': mensaje})
+
